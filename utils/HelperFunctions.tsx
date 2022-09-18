6954fe4ab55ce8b38bfe780391d1193a849771a5
Work <seebrandt@gmail.com>
2022-09-18 10:59:53 -0400

    Fix ts error

--- utils/HelperFunctions.tsx
+++ utils/HelperFunctions.tsx
@@ -8,6 +8,6 @@ export const getHauntedHouses = async () => {
   return data;
 };
 
-export function classNames(...classes) {
+export function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
