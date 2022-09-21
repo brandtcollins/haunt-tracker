ae5d1f150fa2f919466e972bff655411fe0b2785
Work <seebrandt@gmail.com>
2022-09-21 15:42:41 -0400

    update profile card width

--- pages/index.tsx
+++ pages/index.tsx
@@ -59,7 +59,7 @@ const Home: FunctionComponent<HomeProps> = () => {
         <div className="md:max-w-3xl md:w-4/5">
           <CheckinFeed open={modalIsOpen} setOpen={setModalIsOpen} />
         </div>
-        <div className="px-4 hidden md:block w-full max-w-lg ">
+        <div className="px-4 hidden md:block w-full max-w-md ">
           <ProfileStats />
         </div>
       </div>
