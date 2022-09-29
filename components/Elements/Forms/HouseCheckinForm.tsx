5a9838bac8f788cdc67340b5ab226c86f3e95b52
Work <seebrandt@gmail.com>
2022-09-29 17:21:04 -0400

    Fix default value bug

--- components/Elements/Forms/HouseCheckinForm.tsx
+++ components/Elements/Forms/HouseCheckinForm.tsx
@@ -207,7 +207,6 @@ const HouseCheckinForm: FunctionComponent<HouseCheckinFormProps> = ({
                   id="rating"
                   placeholder="Give your run a rating between 1 and 5"
                   className="mt-1 h-12 block w-full rounded-md bg-darkGray-100 text-white border-darkGray-100 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  defaultValue=""
                   aria-invalid="true"
                   aria-describedby="email-error"
                 />
