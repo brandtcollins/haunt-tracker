3b4dda774e8b76e1c23374d89ebd8bbb184d4305
Work <seebrandt@gmail.com>
2022-09-30 08:11:07 -0400

    Update layout component

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,27 +1,5 @@
-import { Fragment, FunctionComponent, ReactNode } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { FunctionComponent, ReactNode } from "react";
 import Header from "../Header/Header";
-
-const user = {
-  name: "Tom Cook",
-  email: "tom@example.com",
-  imageUrl:
-    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-};
-const navigation = [
-  { name: "Layout", href: "#", current: true },
-  { name: "Team", href: "#", current: false },
-  { name: "Projects", href: "#", current: false },
-  { name: "Calendar", href: "#", current: false },
-  { name: "Reports", href: "#", current: false },
-];
-const userNavigation = [
-  { name: "Your Profile", href: "#" },
-  { name: "Settings", href: "#" },
-  { name: "Sign out", href: "#" },
-];
-
 interface LayoutProps {
   children: ReactNode;
   title: string;
@@ -44,11 +22,9 @@ const Layout: FunctionComponent<LayoutProps> = ({ children, title }) => {
 
         <main className="-mt-32 bg-darkGray-500 min-h-screen">
           <div className="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
-            {/* Replace with your content */}
             <div className="rounded-lg bg-darkGray-300 px-5 py-6 shadow sm:px-6">
               {children}
             </div>
-            {/* /End replace */}
           </div>
         </main>
       </div>
