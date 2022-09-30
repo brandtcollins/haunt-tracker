236f056c193657dcfcfd8ccf841e51b770aab3fb
Work <seebrandt@gmail.com>
2022-09-29 22:32:15 -0400

    Avatar upload

--- pages/user/settings/index.tsx
+++ pages/user/settings/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, FunctionComponent } from "react";
+import { useState, useEffect, FunctionComponent, ChangeEvent } from "react";
 import { supabase } from "../../../utils/supabaseClient";
 import { PaperClipIcon } from "@heroicons/react/20/solid";
 import Layout from "../../../components/Layout/Layout";
@@ -7,6 +7,8 @@ import * as Yup from "yup";
 import { Formik, Field, Form, FormikHelpers,useFormikContext, useFormik, FormikProps,FormikState } from "formik";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { iUserSettings } from "../../../ts/Interfaces";
+import { spawn } from "child_process";
+import { Avatar } from "@chakra-ui/react";
 
 interface UserSettingsProps {
   session: any;
@@ -18,6 +20,7 @@ const UserSettings: FunctionComponent<UserSettingsProps> = ({ session }) => {
   const [website, setWebsite] = useState<string | null>(null);
   const [avatar_url, setAvatarUrl] = useState<string | null>(null);
   const [formDisabled, setFormDisabled] = useState(true);
+  const [changeAvatar, setChangeAvatar] = useState(false);
   const queryClient = useQueryClient();
 
   useEffect(() => {
@@ -119,6 +122,17 @@ const UserSettings: FunctionComponent<UserSettingsProps> = ({ session }) => {
     },
   });
 
+  const handleUpload = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      console.log(event.target.files[0]);
+    }
+  };
+
+  const handleFormCancel = () => {
+    setFormDisabled(!formDisabled);
+    setChangeAvatar(false);
+  };
+
   return (
     <Layout title="User Settings">
       {!loading && (
@@ -136,7 +150,7 @@ const UserSettings: FunctionComponent<UserSettingsProps> = ({ session }) => {
                     User Settings
                   </h3>
                   <h3
-                    onClick={() => setFormDisabled(!formDisabled)}
+                    onClick={() => handleFormCancel()}
                     className="text-lg font-medium leading-6 text-emerald-500 hover:text-emerald-600 cursor-pointer"
                   >
                     {formDisabled ? "Edit" : "Cancel"}
@@ -180,15 +194,21 @@ const UserSettings: FunctionComponent<UserSettingsProps> = ({ session }) => {
                       <dt className="text-sm font-medium text-gray-500">
                         Avatar
                       </dt>
-                      <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                        <Field
-                          name="avatar_url"
-                          disabled={formDisabled}
-                          id="avatar_url"
-                          placeholder="Avatar URL"
-                          aria-invalid="true"
-                          className="w-full"
-                          aria-describedby="avatar_url-error"
+                      <dd
+                        className={`mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0`}
+                      >
+                        <UploadAvatar
+                          url={avatar_url}
+                          size={75}
+                          formDisabled={formDisabled}
+                          onUpload={(url: string) => {
+                            setAvatarUrl(url);
+                            updateProfile({
+                              username,
+                              website,
+                              avatar_url: url,
+                            });
+                          }}
                         />
                       </dd>
                     </div>
@@ -208,6 +228,108 @@ const UserSettings: FunctionComponent<UserSettingsProps> = ({ session }) => {
         </Formik>
       )}
     </Layout>
+  );
+};
+
+interface UploadAvatarProps {
+  url: string | null;
+  size: number;
+  onUpload: any;
+  formDisabled: boolean;
+}
+
+const UploadAvatar: FunctionComponent<UploadAvatarProps> = ({
+  url,
+  size,
+  onUpload,
+  formDisabled,
+}) => {
+  const [avatarUrl, setAvatarUrl] = useState<string | null>(null);
+  const [uploading, setUploading] = useState<boolean>(false);
+  const [changeAvatar, setChangeAvatar] = useState(false);
+
+  useEffect(() => {
+    if (url) downloadImage(url);
+  }, [url]);
+
+  async function downloadImage(path: string) {
+    try {
+      const { data, error } = await supabase.storage
+        .from("avatars")
+        .download(path);
+      if (error) {
+        throw error;
+      }
+      const url = URL.createObjectURL(data);
+      setAvatarUrl(url);
+    } catch (error: any) {
+      console.log("Error downloading image: ", error.message);
+    }
+  }
+
+  async function uploadAvatar(event: ChangeEvent<HTMLInputElement>) {
+    try {
+      setUploading(true);
+
+      if (!event.target.files || event.target.files.length === 0) {
+        throw new Error("You must select an image to upload.");
+      }
+
+      const file = event.target.files[0];
+      const fileExt = file.name.split(".").pop();
+      const fileName = `${Math.random()}.${fileExt}`;
+      const filePath = `${fileName}`;
+
+      let { error: uploadError } = await supabase.storage
+        .from("avatars")
+        .upload(filePath, file);
+
+      if (uploadError) {
+        throw uploadError;
+      }
+
+      onUpload(filePath);
+    } catch (error: any) {
+      alert(error.message);
+    } finally {
+      setUploading(false);
+    }
+  }
+
+  return (
+    <div className="flex">
+      {avatarUrl ? (
+        <img
+          src={avatarUrl}
+          alt="Avatar"
+          className="avatar image"
+          style={{ height: size, width: size }}
+        />
+      ) : (
+        <div
+          className="avatar no-image"
+          style={{ height: size, width: size }}
+        />
+      )}
+      <div className="pl-6">
+        <h1
+          onClick={() => setChangeAvatar(true)}
+          className={`font-bold text-emerald-500 hover:cursor-pointer pl-4 ${
+            (formDisabled || changeAvatar) && "hidden"
+          }`}
+        >
+          Change Avatar
+        </h1>
+        <input
+          className={`${(formDisabled || !changeAvatar) && "hidden"}`}
+          type="file"
+          id="single"
+          accept="image/*"
+          onChange={uploadAvatar}
+          disabled={uploading}
+        />
+      </div>
+    </div>
   );
 };
 
