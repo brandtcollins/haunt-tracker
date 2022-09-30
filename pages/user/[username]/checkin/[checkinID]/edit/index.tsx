d87dad0256007eb44113cfebd376cd72feb743f5
Work <seebrandt@gmail.com>
2022-09-30 17:44:47 -0400

    Code cleanup

--- pages/user/[username]/checkin/[checkinID]/edit/index.tsx
+++ pages/user/[username]/checkin/[checkinID]/edit/index.tsx
@@ -1,10 +1,6 @@
-import { User } from "@supabase/supabase-js";
-import { useQuery } from "@tanstack/react-query";
-import { useRouter } from "next/router";
-import { FunctionComponent, useState } from "react";
+import { FunctionComponent } from "react";
 import HouseCheckinForm from "../../../../../../components/Elements/Forms/HouseCheckinForm";
 import Layout from "../../../../../../components/Layout/Layout";
-import { supabase } from "../../../../../../utils/supabaseClient";
 
 interface EditCheckinProps {}
 
--- pages/user/[username]/checkin/index.tsx
+++ pages/user/[username]/checkin/index.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useState } from "react";
+import { FunctionComponent } from "react";
 import HouseCheckinForm from "../../../../components/Elements/Forms/HouseCheckinForm";
 import Layout from "../../../../components/Layout/Layout";
 
