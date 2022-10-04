bc87e8e543a599a9ac1f979030924f2951bdce29
Work <seebrandt@gmail.com>
2022-10-03 22:53:30 -0400

    update password reset page

--- components/Elements/Forms/Auth/PasswordResetForm.tsx
+++ components/Elements/Forms/Auth/PasswordResetForm.tsx
@@ -3,39 +3,43 @@ import { Formik, Field, Form, FormikState } from "formik";
 import { useRouter } from "next/router";
 import { iUserSignIn } from "../../../../ts/Interfaces";
 import { supabase } from "../../../../utils/supabaseClient";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function SignInForm() {
-  interface iPasswordReset {
-    email: string;
-  }
-  const initialValues: iPasswordReset = {
-    email: "",
-  };
+  const [passwordRecovery, setPasswordRecovery] = useState<boolean>(false);
 
-  const validationSchema = Yup.object().shape({
+  const emailValidationSchema = Yup.object().shape({
     email: Yup.string().email().required("Email is a required field"),
   });
 
-  const router = useRouter();
+  const passwordValidationSchema = Yup.object().shape({
+    password: Yup.string()
+      .required("Please enter your password")
+      .matches(
+        /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
+        "Password must contain at least 8 characters, one uppercase, one number and one special case character"
+      ),
+    confirmPassword: Yup.string()
+      .required("Please confirm your password")
+      .when("password", {
+        is: (password: string) =>
+          password && password.length > 0 ? true : false,
+        then: Yup.string().oneOf(
+          [Yup.ref("password")],
+          "Password doesn't match"
+        ),
+      }),
+  });
 
   useEffect(() => {
     supabase.auth.onAuthStateChange(async (event, session) => {
       if (event == "PASSWORD_RECOVERY") {
-        const newPassword: string | undefined = prompt(
-          "What would you like your new password to be?"
-        )!;
-        const { data, error } = await supabase.auth.updateUser({
-          password: newPassword,
-        });
-
-        if (data) alert("Password updated successfully!");
-        if (error) alert("There was an error updating your password.");
+        setPasswordRecovery(true);
       }
     });
   }, []);
 
-  const handlePasswordReset = async (values: iPasswordReset) => {
+  const handlePasswordResetRequest = async (values: { email: string }) => {
     const { data, error } = await supabase.auth.resetPasswordForEmail(
       values.email,
       {
@@ -44,14 +48,83 @@ export default function SignInForm() {
     );
   };
 
+  const handlePasswordReset = async (values: {
+    password: string;
+    confirmPassword: string;
+  }) => {
+    const { data, error } = await supabase.auth.updateUser({
+      password: values.password,
+    });
+
+    if (data) alert("Password updated successfully!");
+    if (error) alert("There was an error updating your password.");
+  };
+
+  if (passwordRecovery) {
+    return (
+      <>
+        <Formik
+          initialValues={{ password: "", confirmPassword: "" }}
+          onSubmit={(values) => handlePasswordReset(values)}
+          validationSchema={passwordValidationSchema}
+        >
+          {({
+            errors,
+            touched,
+            isSubmitting,
+          }: FormikState<{ password: string }>) => (
+            <Form className="text-white">
+              {isSubmitting && <p>Submitting</p>}
+              <div>
+                <label htmlFor="password" className="block font-medium">
+                  Password
+                </label>
+                <div className="mt-1">
+                  <Field
+                    name="password"
+                    id="password"
+                    placeholder=""
+                    className="mt-1 h-12 block w-full rounded-md bg-darkGray-100 text-white border-darkGray-100 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    aria-invalid="true"
+                    aria-describedby="email-error"
+                  />
+                  {errors.password && touched.password ? (
+                    <p
+                      className="mt-2 text-sm text-emerald-500"
+                      id="email-error"
+                    >
+                      {errors.password}
+                    </p>
+                  ) : null}
+                </div>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="mt-10 flex w-full justify-center rounded-md border border-transparent bg-emerald-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-emerald-600 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-offset-2"
+                >
+                  Sign In
+                </button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </>
+    );
+  }
+
   return (
     <>
       <Formik
-        initialValues={initialValues}
-        onSubmit={(values) => handlePasswordReset(values)}
-        validationSchema={validationSchema}
+        initialValues={{ email: "" }}
+        onSubmit={(values) => handlePasswordResetRequest(values)}
+        validationSchema={emailValidationSchema}
       >
-        {({ errors, touched, isSubmitting }: FormikState<iPasswordReset>) => (
+        {({
+          errors,
+          touched,
+          isSubmitting,
+        }: FormikState<{ email: string }>) => (
           <Form className="text-white">
             {isSubmitting && <p>Submitting</p>}
             <div>
