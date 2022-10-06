accf509bd1079594211fab892068c68dd20bb72e
Work <seebrandt@gmail.com>
2022-10-05 21:14:03 -0400

    Fix loading animation

--- pages/haunts/index.tsx
+++ pages/haunts/index.tsx
@@ -56,20 +56,20 @@ const Haunts: FunctionComponent<HauntsProps> = () => {
     }
   );
 
-  if (hauntsAreLoading) {
-    return <LoadingCircle />;
-  }
-
   return (
     <Layout title="Haunts">
-      <ul
-        role="list"
-        className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
-      >
-        {haunts?.map((haunt, index) => (
-          <HauntCard haunt={haunt} key={index} />
-        ))}
-      </ul>
+      {hauntsAreLoading ? (
+        <LoadingCircle />
+      ) : (
+        <ul
+          role="list"
+          className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+        >
+          {haunts?.map((haunt, index) => (
+            <HauntCard haunt={haunt} key={index} />
+          ))}
+        </ul>
+      )}
     </Layout>
   );
 };
